144d68c5119eb478853da3608c7ce544af386715
Ibrohim Umarxo'jayev <76086219+IbrohimUmar@users.noreply.github.com>
2022-09-08 18:16:42 +0500

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,9 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         next_new = 0
-        for i in range(len(nums)):
-            if val != nums[i]:
-                nums[next_new] = nums[i]
+        for i, num in enumerate(nums):
+            if val != num:
+                nums[next_new] = num
                 next_new += 1
-
         return next_new
